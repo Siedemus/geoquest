0794d540263468bb089a45bf5baa2ec88ae9c455
Siedemus <barsak1177@gmail.com>
2024-05-28 22:50:40 +0200

    change button text color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,15 +10,15 @@ const Hero = () => {
         from images.
       </p>
       {true ? (
-        <button className="bg-pastelRainbowButton text-cosmic px-4 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s] flex gap-2 items-center">
+        <button className="bg-pastelRainbowButton text-noise px-4 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s] flex gap-2 items-center">
           Log In with <img src="src/assets/images/google.png" className="w-6" />
         </button>
       ) : (
         <div className="flex gap-5">
-          <button className="bg-pastelRainbowButton text-cosmic px-8 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s]">
+          <button className="bg-pastelRainbowButton text-noise px-8 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s]">
             Play
           </button>
-          <button className="bg-pastelRainbowButton text-cosmic px-8 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s]">
+          <button className="bg-pastelRainbowButton text-noise px-8 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s]">
             Log Out
           </button>
         </div>
