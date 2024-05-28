236f2003f97c7334415811c0037b21bd39df8c6e
Siedemus <barsak1177@gmail.com>
2024-05-28 13:39:36 +0200

    add main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 function App() {
   return (
     <>
-      <div className="bg-red-600 text-3xl">hello</div>
+      <main className="flex flex-col items-center mt-24 md:mt-48 text-center p-2 pb-0 gap-6">
+        <h2 className="bg-pastelRainbow text-transparent bg-clip-text font-extrabold text-5xl md:text-7xl max-w-7xl animate-pastel">
+          Challenge Your World Knowledge and Discover New Places!
+        </h2>
+        <p className="text-noise text-xl max-w-[520px]">
+          GeoQuest is an interactive geography-based game where players are
+          challenged to identify locations around the world based on visual
+          clues from images.
+        </p>
+        {true ? (
+          <button className="bg-pastelRainbowButton text-cosmic px-4 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s] flex gap-2 items-center">
+            Log In with{" "}
+            <img src="src/assets/images/google.png" className="w-6" />
+          </button>
+        ) : (
+          <div className="flex gap-5">
+            <button className="bg-pastelRainbowButton text-cosmic px-8 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s]">
+              Play
+            </button>
+            <button className="bg-pastelRainbowButton text-cosmic px-8 py-2 rounded-md text-lg font-bold bg-left bg-buttonZoom hover:bg-right duration-[1.5s]">
+              Log Out
+            </button>
+          </div>
+        )}
+        <img src="src/assets/images/planet.png" />
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,42 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cosmic: "#000000",
+        cloudy: "#FFFFFF",
+        noise: "#737373",
+        salt: "#0F0F0F",
+      },
+      backgroundSize: {
+        buttonZoom: "700%",
+      },
+      fontFamily: {
+        gabarito: ["Gabarito", "sans-serif"],
+        libreFranklin: ["Libre Franklin", "sans-serif"],
+      },
+      backgroundImage: {
+        pastelRainbow:
+          "linear-gradient(139deg, rgba(240,158,167,1) 0%, rgba(246,202,148,1) 16%, rgba(250,250,190,1) 33%, rgba(244,248,190,1) 34%, rgba(193,235,192,1) 51%, rgba(199,202,255,1) 65%, rgba(205,171,235,1) 83%, rgba(246,194,243,1) 100%)",
+        pastelRainbowButton:
+          "linear-gradient(295deg, rgba(255,255,255,1) 0%, rgba(240,158,167,1) 43%, rgba(246,202,148,1) 45%, rgba(250,250,190,1) 47%, rgba(244,248,190,1) 49%, rgba(193,235,192,1) 51%, rgba(199,202,255,1) 53%, rgba(205,171,235,1) 55%, rgba(246,194,243,1) 56%, rgba(255,255,255,1) 100%)",
+      },
+      animation: {
+        pastel: "pastel 8s ease infinite",
+      },
+      keyframes: {
+        pastel: {
+          "0%, 100%": {
+            "background-size": "350%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "350%",
+            "background-position": "center right",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
