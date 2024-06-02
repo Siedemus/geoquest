d79659dd6a0a2accb175c43f0479063da63ec72e
Siedemus <barsak1177@gmail.com>
2024-06-02 19:44:21 +0200

    add user photo and home link

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,9 +4,9 @@ const Navigation = () => {
   const auth = useAuthContext();
 
   return !auth?.pending ? (
-    <header className="flex justify-between p-3 md:p-4 bg-salt fixed top-0 w-full bg-opacity-80 items-center">
+    <header className="z-10 flex justify-between p-3 md:p-4 bg-salt fixed top-0 w-full bg-opacity-80 items-center">
       <h1 className="bg-pastelRainbow text-transparent bg-clip-text font-extrabold text-4xl animate-pastel">
-        goeQuest
+        <a href="/">geoQuest</a>
       </h1>
       <nav className="">
         <ul className="flex gap-5 items-center">
@@ -21,7 +21,10 @@ const Navigation = () => {
                 </button>
               </li>
               <li className="border-2 rounded-full">
-                <img className="rounded-full w-10 p-0.5" src={auth.user.photoURL!} />
+                <img
+                  className="rounded-full w-10 p-0.5"
+                  src={auth.user.photoURL!}
+                />
               </li>
             </>
           ) : (
