9ed984c551935f8eda9b7f0a2f0b8b7bba5f345f
Siedemus <barsak1177@gmail.com>
2024-06-07 01:06:22 +0200

    add PositionState Interface

--- src/assets/resources/interfaces.ts
+++ src/assets/resources/interfaces.ts
@@ -1,6 +1,13 @@
+import { LatLng } from "leaflet";
+
 export interface ILeaderboard {
   rank: number;
   name: string;
   score: number;
   location: string;
+}
+
+export interface IPositionState {
+  selectedPosition: LatLng | null;
+  setSelectedPosition: React.Dispatch<React.SetStateAction<LatLng | null>>;
 }
