58af7fc7ac717363bc42b4439a6c9f0ba6c1e544
Siedemus <barsak1177@gmail.com>
2024-06-09 01:37:16 +0200

    create useDistanceInKm hook

---
+++ src/assets/hooks/useDistanceInKm.ts
@@ -0,0 +1,25 @@
+import { LatLng } from "leaflet";
+
+const useDistanceInKm = (
+  selectedPosition: LatLng,
+  initialPosition: { lat: number; lng: number }
+) => {
+  const R = 6371;
+  const dLat = deg2rad(selectedPosition.lat - initialPosition.lat);
+  const dLng = deg2rad(selectedPosition.lng - initialPosition.lng);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(initialPosition.lat)) *
+      Math.cos(deg2rad(selectedPosition.lat)) *
+      Math.sin(dLng / 2) *
+      Math.sin(dLng / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+  return Math.floor(distance);
+};
+
+const deg2rad = (deg: number) => {
+  return deg * (Math.PI / 180);
+};
+
+export default useDistanceInKm;
