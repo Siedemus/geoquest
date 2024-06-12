0607568f9cda187e6064e2e11525e7335464c0b8
Siedemus <barsak1177@gmail.com>
2024-06-12 23:59:47 +0200

    add new hooks: useGetBestScores and useGetRank

---
+++ src/assets/hooks/useGetBestScores.ts
@@ -0,0 +1,65 @@
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { useEffect, useState } from "react";
+
+const useGetBestScores = (): [
+  bestScores: {
+    rank: number;
+    name: string;
+    score: number;
+    position: string;
+  }[],
+  isPending: boolean,
+  error: null | string
+] => {
+  const [bestScores, setBestScores] = useState<
+    {
+      rank: number;
+      name: string;
+      score: number;
+      position: string;
+    }[]
+  >([]);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchBestScores = async () => {
+      try {
+        const q = query(
+          collection(db, "scores"),
+          orderBy("score", "desc"),
+          orderBy("date", "desc"),
+          limit(5)
+        );
+        const snapshot = await getDocs(q);
+        const scores = snapshot.docs.map((doc, i) => {
+          const data = doc.data();
+          return {
+            rank: i + 1,
+            name: data.name,
+            score: data.score,
+            position: data.position,
+          };
+        });
+        setBestScores(scores);
+      } catch (e: any) {
+        setError(e.message || "Sorry but we cant get actual leaderboard.");
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchBestScores();
+  }, []);
+
+  return [bestScores, isPending, error];
+};
+
+export default useGetBestScores;
---
+++ src/assets/hooks/useGetRank.ts
@@ -0,0 +1,44 @@
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { useEffect, useState } from "react";
+
+const useGetRank = (
+  userUid: string,
+  date: number
+): [rank: number, isPending: boolean, error: null | string] => {
+  const [rank, setRank] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState<null | string>(null);
+
+  useEffect(() => {
+    const getRank = async () => {
+      try {
+        setIsPending(true);
+        const q = query(
+          collection(db, "scores"),
+          where("userUid", "==", userUid),
+          orderBy("score", "desc")
+        );
+
+        const snapshot = await getDocs(q);
+
+        const rank = snapshot.docs.findIndex((doc) => {
+          const data = doc.data();
+          return data.userUid === userUid && data.date === date;
+        });
+
+        setRank(rank + 1);
+      } catch (e: any) {
+        setError(e.message || "Something went wrong.");
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    getRank();
+  }, []);
+
+  return [rank, isPending, error];
+};
+
+export default useGetRank;
