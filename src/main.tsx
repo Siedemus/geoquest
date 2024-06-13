b13d635162c0a1a89b657d7d5dd5f11761cf583d
Siedemus <barsak1177@gmail.com>
2024-06-14 01:52:56 +0200

    add loadingelement to loadscript component, prevent google fonts loading, add async script loading

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import RouteError from "./components/RouteError.tsx";
 import Game from "./pages/game/Game.tsx";
 import { Toaster } from "sonner";
 import { LoadScript } from "@react-google-maps/api";
+import { Commet } from "react-loading-indicators";
 
 const router = createBrowserRouter([
   {
@@ -24,7 +25,15 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <LoadScript googleMapsApiKey="AIzaSyAcKzJ9-X_B0NVUMM1t1lxbmJ2_O9VUib4">
+    <LoadScript
+      preventGoogleFontsLoading={true}
+      loadingElement={
+        <div className="mx-auto my-80 w-max">
+          <Commet color="lightBlue" size="medium" />
+        </div>
+      }
+      googleMapsApiKey="AIzaSyAcKzJ9-X_B0NVUMM1t1lxbmJ2_O9VUib4&loading=async"
+    >
       <AuthProvider>
         <Navigation />
         <RouterProvider router={router} />
