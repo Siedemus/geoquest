0755b926af80a593fa55278f0eab54f73bd9f646
Siedemus <barsak1177@gmail.com>
2024-06-17 00:26:49 +0200

    handle not authorized user

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -4,23 +4,37 @@ import ScoreContainer from "./ScoreContainer";
 import StreetViewContainer from "./StreetViewContainer";
 import PlaceSelector from "./PlaceSelector";
 import ErrorComponent from "../../components/ErrorComponent";
+import { useAuthContext } from "../../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Game = () => {
   const gameContext = useContext(GameContext);
+  const auth = useAuthContext();
+  const navigate = useNavigate();
   const googleMapsApiErrorMessage =
     "There was an issue loading the Google Maps. Please try again later.";
 
-  return gameContext !== undefined ? (
-    gameContext.showScore ? (
-      <ScoreContainer />
+  const handleNotAuthorizedUser = () => {
+    navigate("/");
+    toast.warning("You are not logged in.");
+  };
+
+  return auth?.user ? (
+    gameContext !== undefined ? (
+      gameContext.showScore ? (
+        <ScoreContainer />
+      ) : (
+        <>
+          <StreetViewContainer />
+          <PlaceSelector />
+        </>
+      )
     ) : (
-      <>
-        <StreetViewContainer />
-        <PlaceSelector />
-      </>
+      <ErrorComponent errorMessage={googleMapsApiErrorMessage} />
     )
   ) : (
-    <ErrorComponent errorMessage={googleMapsApiErrorMessage} />
+    handleNotAuthorizedUser()
   );
 };
 
